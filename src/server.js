63cddbcd1f356f2c733f829f31d3710d21618e05
VarunGotmare <varunpgotmare@gmail.com>
2025-12-15 02:28:29 +0530

    final cors fix v1

--- src/server.js
+++ src/server.js
@@ -8,18 +8,27 @@ const sweetsRoutes = require('./routes/sweets.routes');
 
 const app = express();
 
-app.use(cors(
-  {
-    origin: [
-      "http://localhost:4000", //frontend local
-      "sweet-mgmt-frontend-production.up.railway.app",   //frontend production
-    ],
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      // allow requests with no origin (Postman, curl)
+      if (!origin) return callback(null, true);
+
+      const allowedOrigins = [
+        "http://localhost:5173",
+        "https://sweet-mgmt-frontend-production.up.railway.app",
+      ];
+
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("CORS not allowed"));
+      }
+    },
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-  }
-));
-app.options(/.*/, cors());
+  })
+);
 app.use(express.json());
 
 app.get('/health', async (req, res) => {
